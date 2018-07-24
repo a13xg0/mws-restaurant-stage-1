a0746c366cf783de8b1b4ba24b4daaa29c7244a0
Alex Gorbach <a.v.gorbach@gmail.com>
2018-07-24 15:02:55 +1000

    feat: first iteration of a11y optimisation
    optimized for keyboard users

--- js/main.js
+++ js/main.js
@@ -177,6 +177,8 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role', 'button');
+  more.setAttribute('aria-label', `View details for ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more);
 
@@ -191,6 +193,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
     marker.on("click", onClick);
+    marker.on("keypress", onClick);
     function onClick() {
       window.location.href = marker.options.url;
     }
